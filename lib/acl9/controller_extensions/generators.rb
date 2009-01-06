4d9d8e5cda5f9fe411ceb516c0365b781881d826
oleg dashevskii <be9@be9.ru>
2009-01-06 16:26:35 +0600

    DRY generators

--- lib/acl9/controller_extensions/generators.rb
+++ lib/acl9/controller_extensions/generators.rb
@@ -6,13 +6,43 @@ module Acl9
 
   module Dsl
     module Generators
+      class BaseGenerator < Acl9::Dsl::Base
+        def initialize(*args)
+          @subject_method = args[0]
 
-      class FilterLambda < Acl9::Dsl::Base
+          super
+        end
+
+        protected
+
+        def _access_denied
+          "raise Acl9::AccessDenied"
+        end
+
+        def _subject_ref
+          "#{_controller_ref}send(:#{@subject_method})"
+        end
+
+        def _object_ref(object)
+          "#{_controller_ref}instance_variable_get('@#{object}')"
+        end
+
+        def _action_ref
+          "#{_controller_ref}action_name"
+        end
+
+        def _controller_ref
+          @controller ? "#{@controller}." : ''
+        end
+      end
+
+      class FilterLambda < BaseGenerator
         def initialize(subject_method)
           super
-          @subject_method = subject_method
+
+          @controller = 'controller'
         end
-        
+
         def install_on(controller_class, options)
           controller_class.send(:before_filter, options, &self.to_proc)
         end
@@ -30,38 +60,26 @@ module Acl9
         rescue SyntaxError
           raise FilterSyntaxError, code
         end
-
-        protected 
-        
-        def _access_denied
-          "raise Acl9::AccessDenied"
-        end
-
-        def _subject_ref
-          "controller.send(:#{@subject_method})"
-        end
-
-        def _object_ref(object)
-          "controller.instance_variable_get('@#{object}')"
-        end
-
-        def _action_ref
-          "controller.action_name"
-        end
       end
       
-      class FilterMethod < Acl9::Dsl::Base
+      class FilterMethod < BaseGenerator
         def initialize(subject_method, method_name)
           super
-          @subject_method = subject_method
+
           @method_name = method_name
+          @controller = nil
         end
         
         def install_on(controller_class, options)
-          code = self.to_method_code
-          controller_class.send(:class_eval, code)
+          _add_method(controller_class)
           controller_class.send(:before_filter, @method_name, options)
-        
+        end
+
+        protected
+
+        def _add_method(controller_class)
+          code = self.to_method_code
+          controller_class.send(:class_eval, code, __FILE__, __LINE__)
         rescue SyntaxError
           raise FilterSyntaxError, code
         end
@@ -69,44 +87,20 @@ module Acl9
         def to_method_code
           <<-RUBY
             def #{@method_name}
-              unless #{allowance_expression}; #{_access_denied}; end
+              unless #{allowance_expression}
+                #{_access_denied}
+              end
             end
           RUBY
         end
-
-        protected 
-        
-        def _access_denied
-          "raise Acl9::AccessDenied"
-        end
-
-        def _subject_ref
-          "send(:#{@subject_method})"
-        end
-
-        def _object_ref(object)
-          "instance_variable_get('@#{object}')"
-        end
-
-        def _action_ref
-          "action_name"
-        end
       end
       
-      class BooleanMethod < Acl9::Dsl::Base
-        def initialize(subject_method, method_name)
-          super
-          @subject_method = subject_method
-          @method_name = method_name
-        end
-        
+      class BooleanMethod < FilterMethod
         def install_on(controller_class, *_)
-          code = self.to_method_code
-          controller_class.send(:class_eval, code)
-        
-        rescue SyntaxError
-          raise FilterSyntaxError, code
+          _add_method(controller_class)
         end
+
+        protected 
         
         def to_method_code
           <<-RUBY
@@ -116,22 +110,8 @@ module Acl9
           RUBY
         end
 
-        protected 
-        
-        def _access_denied
-          "raise Acl9::AccessDenied"
-        end
-
-        def _subject_ref
-          "send(:#{@subject_method})"
-        end
-
         def _object_ref(object)
-          "(options[:#{object}] || instance_variable_get('@#{object}'))"
-        end
-
-        def _action_ref
-          "action_name"
+          "(options[:#{object}] || #{super})"
         end
       end
     end
