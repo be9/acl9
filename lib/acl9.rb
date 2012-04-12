f65e55e9953ff97dfae813dfd4c1ffafeb9bd077
Enrique Vidal <enrique@cloverinteractive.com>
2012-04-11 22:01:57 -0700

    Make Acl9 less tolerant about it's dependencies

--- lib/acl9.rb
+++ lib/acl9.rb
@@ -1,16 +1,20 @@
-require File.join(File.dirname(__FILE__), 'acl9', 'config')
+require 'acl9/version'
+require 'acl9/model_extensions'
+require 'acl9/controller_extensions'
+require 'acl9/helpers'
 
-if defined? ActiveRecord::Base
-  require File.join(File.dirname(__FILE__), 'acl9', 'model_extensions')
+module Acl9
+  @@config = {
+    :default_role_class_name    => 'Role',
+    :default_subject_class_name => 'User',
+    :default_subject_method     => :current_user,
+    :default_association_name   => :role_objects,
+    :protect_global_roles       => false,
+  }
 
-  ActiveRecord::Base.send(:include, Acl9::ModelExtensions)
+  mattr_reader :config
 end
 
-
-if defined? ActionController::Base
-  require File.join(File.dirname(__FILE__), 'acl9', 'controller_extensions')
-  require File.join(File.dirname(__FILE__), 'acl9', 'helpers')
-
-  ActionController::Base.send(:include, Acl9::ControllerExtensions)
-  Acl9Helpers = Acl9::Helpers unless defined?(Acl9Helpers)
-end
+ActiveRecord::Base.send(:include, Acl9::ModelExtensions)
+ActionController::Base.send(:include, Acl9::ControllerExtensions)
+Acl9Helpers = Acl9::Helpers unless defined?(Acl9Helpers)
