422a452612bdde59c1414afeca55d0c7dc0509ca
Jason King <jk@handle.it>
2014-11-18 09:57:47 -0800

    Subject has_many working, fixes #82

--- lib/acl9/model_extensions.rb
+++ lib/acl9/model_extensions.rb
@@ -73,33 +73,12 @@ module Acl9
       def acts_as_authorization_object(options = {})
         subject = options[:subject_class_name] || Acl9::config[:default_subject_class_name]
         subj_table = subject.constantize.table_name
-        subj_col = subject.underscore
 
-        role       = options[:role_class_name] || Acl9::config[:default_role_class_name]
-        role_table = role.constantize.table_name
-
-        join_table = options[:join_table_name]
-        join_table ||= ActiveRecord::Base.send(:join_table_name,
-          role_table, subj_table) if ActiveRecord::Base.private_methods \
-          .include?('join_table_name')
-        join_table ||= Acl9::config[:default_join_table_name]
-        join_table ||= self.table_name_prefix \
-            + [undecorated_table_name(self.to_s),
-            undecorated_table_name(role)].sort.join("_") \
-            + self.table_name_suffix
+        role = options[:role_class_name] || Acl9::config[:default_role_class_name]
 
         has_many :accepted_roles, :as => :authorizable, :class_name => role, :dependent => :destroy
 
-        has_many :"#{subj_table}",
-          :finder_sql => proc { "SELECT DISTINCT #{subj_table}.* " +
-                                "FROM #{subj_table} INNER JOIN #{join_table} ON #{subj_col}_id = #{subj_table}.id " +
-                                "INNER JOIN #{role_table} ON #{role_table}.id = #{role.underscore}_id " +
-                                "WHERE authorizable_type = '#{self.class.base_class.to_s}' AND authorizable_id = #{id} "},
-          :counter_sql => proc { "SELECT COUNT(DISTINCT #{subj_table}.id)" + 
-                                 "FROM #{subj_table} INNER JOIN #{join_table} ON #{subj_col}_id = #{subj_table}.id " +
-                                 "INNER JOIN #{role_table} ON #{role_table}.id = #{role.underscore}_id " +
-                                 "WHERE authorizable_type = '#{self.class.base_class.to_s}' AND authorizable_id = #{id} "},
-          :readonly => true
+        has_many :"#{subj_table}", -> { distinct.readonly }, through: :accepted_roles
 
         include Acl9::ModelExtensions::ForObject
       end
