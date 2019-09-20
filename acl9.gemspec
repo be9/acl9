e5c407581285e24e049d15e7847b8cacd7e68e08
Martijn Casteel <martijn.casteel@gmail.com>
2019-09-20 22:49:42 +0200

    Improved migrator deprecation and creation of MigrationContext

--- acl9.gemspec
+++ acl9.gemspec
@@ -23,5 +23,5 @@ Gem::Specification.new do |s|
   s.add_dependency "rails", '> 5.0', '< 7.0'
 
   s.add_development_dependency "yard"
-  s.add_development_dependency "sqlite3"
+  s.add_development_dependency "sqlite3", "~> 1.3.6"
 end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -9,8 +9,10 @@ Rails.backtrace_cleaner.remove_silencers! if ENV["BACKTRACE"]
 
 ActiveRecord::Migration.verbose = false
 
-if Rails.gem_version >= Gem::Version.new('5.2.0')
+if Rails.gem_version >= Gem::Version.new('6.0')
   ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __FILE__), ActiveRecord::SchemaMigration).migrate
+elsif Rails.gem_version >= Gem::Version.new('5.2.0')
+  ActiveRecord::MigrationContext.new(File.expand_path('../dummy/db/migrate', __FILE__)).migrate
 else
   ActiveRecord::Migrator.migrate(File.expand_path('../dummy/db/migrate', __FILE__))
 end
